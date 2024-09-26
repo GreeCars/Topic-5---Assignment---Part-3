e9166af0705406d3ed81490b94aec29d459c5314
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-26 09:39:04 -0400

    Working

--- Topic 5 - Assignment - Part 3/Program.cs
+++ Topic 5 - Assignment - Part 3/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Carsen
+Console.WriteLine("Part 3 - Mini Quiz");
+Console.WriteLine();
+
+string waterElement;
+int score, independence;
+score = 0;
+
+Console.WriteLine("How many years has it been since Canada gained independence?");
+Int32.TryParse(Console.ReadLine(), out independence);
+if (independence == 157)
+{
+    Console.WriteLine("That's correct.");
+    score += 1;
+}
+else if (independence > 157)
+    Console.WriteLine("That's too high.");
+else if (independence < 157)
+    Console.WriteLine("That's too low.");
+
+Console.WriteLine("What element is combined with hydrogen to make water?");
+waterElement = Console.ReadLine();
+if (waterElement.ToLower() == "oxygen")
+{
+    Console.WriteLine("That's correct.");
+    score += 1;
+}
+else
+    Console.WriteLine("That's not the right element.");
