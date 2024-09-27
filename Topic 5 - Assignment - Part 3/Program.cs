4f1c375225b381195313f47219b13912f973a753
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-09-27 09:38:18 -0400

    Working

--- Topic 5 - Assignment - Part 3/Program.cs
+++ Topic 5 - Assignment - Part 3/Program.cs
@@ -3,8 +3,9 @@
 Console.WriteLine("Part 3 - Mini Quiz");
 Console.WriteLine();
 
-string waterElement;
-int score, independence;
+string waterElement, xperia, brandenburg;
+int independence;
+double score;
 score = 0;
 
 Console.WriteLine("How many years has it been since Canada gained independence?");
@@ -18,13 +19,61 @@ else if (independence > 157)
     Console.WriteLine("That's too high.");
 else if (independence < 157)
     Console.WriteLine("That's too low.");
+Console.WriteLine();
 
 Console.WriteLine("What element is combined with hydrogen to make water?");
 waterElement = Console.ReadLine();
-if (waterElement.ToLower() == "oxygen")
+if (waterElement.ToLower() == "oxygen" || waterElement.ToUpper() == "O2")
 {
     Console.WriteLine("That's correct.");
     score += 1;
 }
 else
     Console.WriteLine("That's not the right element.");
+Console.WriteLine();
+
+Console.WriteLine("What company makes the Xperia model of smartphone?");
+Console.WriteLine("A. Samsung");
+Console.WriteLine("B. Xiaomi");
+Console.WriteLine("C. Sony");
+Console.WriteLine("D. Vivo");
+xperia = Console.ReadLine();
+if (xperia.ToLower() == "c" || xperia.ToLower() == "sony")
+{
+    Console.WriteLine("That's correct.");
+    score += 1;
+}
+else if (xperia.ToLower() == "a" || xperia.ToLower() == "samsung")
+{
+    Console.WriteLine("Incorrect, they make the Galaxy model.");
+}
+else if (xperia.ToLower() == "b" || xperia.ToLower() == "xiaomi")
+{
+    Console.WriteLine("Incorrect, they make the Redmi model.");
+}
+else if (xperia.ToLower() == "d" || xperia.ToLower() == "vivo")
+{
+    Console.WriteLine("Incorrect, they name their models after the comany name.");
+}
+else
+    Console.WriteLine("That's an invalid answer.");
+Console.WriteLine();
+
+Console.WriteLine("Vienna is home to the Brandenburg Gate. True or False?");
+brandenburg = Console.ReadLine();
+if (brandenburg.ToLower() == "false" || brandenburg.ToLower() == "f")
+{
+    Console.WriteLine("That's correct.");
+    score += 1;
+}
+else if (brandenburg.ToLower() == "true" || brandenburg.ToLower() == "t")
+    Console.WriteLine("Incorrect, the Brandenbrug Gate is in Berlin.");
+else
+    Console.WriteLine("That's an invalid answer.");
+Console.WriteLine();
+
+Console.WriteLine("You got " + score + " answers correct. That's a score of" + Math.Round(score, 1));
+if (score == 0)
+    Console.WriteLine("You're not too good at this, are you?");
+else if (score == 4)
+    Console.WriteLine("Great job!");
